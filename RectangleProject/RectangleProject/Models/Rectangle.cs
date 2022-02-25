d6d58c15b7a451dc65ca80ed0b1d953f95c4273f
sz1latben <sz1latben@vasvari.org>
2022-02-25 11:14:04 +0100

    kész feladat

---
+++ RectangleProject/RectangleProject/Models/Rectangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RectangleProject.Models
+{
+    public class Rectangle
+    {
+        private double sideA;
+        private double sideB;
+
+        public Rectangle(double sideA, double sideB)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+        }
+
+        public double SideA
+        {
+            get
+            {
+                return this.sideA;
+            }
+            set
+            {
+                this.sideA = value;
+            }
+        }
+        public double SideB
+        {
+            get
+            {
+                return this.sideB;
+            }
+            set
+            {
+                this.sideB = value;
+            }
+        }
+        public double Area
+        {
+            get
+            {
+                return sideA * sideB;
+            }
+        }
+
+        public double Perimeter
+        {
+            get
+            {
+                return 2 * (this.sideA + this.sideB);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "A téglalap egyik oldala: " + sideA + ", a másik oldala: " + sideB + ", a kerülete: " + Math.Round(Perimeter, 2) + ", területe: " + Math.Round(Area, 2) + ".";
+        }
+    }
+}
--- RectangleProject/RectangleProject/Program.cs
+++ RectangleProject/RectangleProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using RectangleProject.Models;
 
 namespace RectangleProject
 {
@@ -6,7 +7,27 @@ namespace RectangleProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle t1 = new Rectangle(2.5, 3.3);
+            Rectangle t2 = new Rectangle(3.5, 4.1);
+
+
+
+
+
+            if (t1.Area > t2.Area)
+            {
+                Console.WriteLine("A nagyobb területű téglalap kerülete: " + t1.Perimeter);
+            }
+
+            else if (t2.Area > t1.Area)
+            {
+                Console.WriteLine("A nagyobb területű téglalap kerülete: " + t2.Perimeter);
+            }
+            else
+            {
+                Console.WriteLine("A két téglalap területe megegyezik! Kerületük:" + t1.Perimeter);
+            }
+                
         }
     }
 }
